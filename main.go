d8da44b16fa25e16641ef72cee56c8444161f9b6
Teodor Yanev <teodor@stacklok.com>
2024-03-22 15:36:17 +0200

    update: test mixed scripts homoglyph

--- main.go
+++ main.go
@@ -2,20 +2,8 @@ package main
 
 import (
 	"fmt"
-	"html"
-	"log"
-	"net/http"
 )
 
 func main() {
-	// comment
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	})
-
-	http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hi There Yolanda!")
-	})
-
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("This is а mixed scriрtㅤwith ")
 }
